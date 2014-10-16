052bca83d04cb43163395aa1c5a2b9e118dfb8f8
planaspa <planaspa@gmail.com>
2014-10-16 12:51:30 +0200

    Using flake8 	- As Vladimir told us I have used flake8 in this file. Now there are 	no warnings. I still have to use flake8 in the rest of the files.

--- src/dbCreator.py
+++ src/dbCreator.py
@@ -3,80 +3,89 @@ import sys
 
 # Verbose mode activation
 if "-v" in sys.argv:
-	verbose = True
-	print "Verbose mode ON."
+    verbose = True
+    print "Verbose mode ON."
 else:
-	verbose = False
-	print "Verbose mode OFF. To activate it use -v parametter when excecuting"
+    verbose = False
+    print "Verbose mode OFF. To activate it use -v parametter when excecuting"
 
 
-#Connection to DataBase
+# Connection to DataBase
 conn = sqlite3.connect('../db/tweetBank.db')
-if verbose : print "Opened database successfully"
+if verbose:
+    print "Opened database successfully"
 
 # Creating main tables
 conn.execute('''CREATE TABLE TWEETS
-       (ID INT PRIMARY KEY NOT NULL,
-       TWEET_TEXT CHAR(140) NOT NULL,
-       FAVS INT NOT NULL,
-       RTS INT NOT NULL,
-       LAT REAL,
-       LONG REAL);''')
+    (ID INT PRIMARY KEY NOT NULL,
+    TWEET_TEXT CHAR(140) NOT NULL,
+    FAVS INT NOT NULL,
+    RTS INT NOT NULL,
+    LAT REAL,
+    LONG REAL);''')
 
-if verbose : print "Table Tweets created successfully"
+if verbose:
+    print "Table Tweets created successfully"
 
+# If verified equals to one means it is true, zero means it is false
 conn.execute('''CREATE TABLE USERS
-       (ID INT PRIMARY KEY NOT NULL,
-       SCREEN_NAME TEXT NOT NULL,
-       NAME TEXT NOT NULL,
-       VERIFIED INT NOT NULL, 
-       LANG TEXT);''') #If verified equals to one means it is true, zero means it is false
+    (ID INT PRIMARY KEY NOT NULL,
+    SCREEN_NAME TEXT NOT NULL,
+    NAME TEXT NOT NULL,
+    VERIFIED INT NOT NULL,
+    LANG TEXT);''')
 
-if verbose : print "Table Users created successfully"
+if verbose:
+    print "Table Users created successfully"
 
 # Creating tables for multivaluated attributes for some tables
 conn.execute('''CREATE TABLE HASHTAGS
-       (HASHTAG TEXT NOT NULL,
-        ID INT NOT NULL,
-	FOREIGN KEY (ID) REFERENCES TWEETS(ID),
-        PRIMARY KEY (HASHTAG, ID));''')
+    (HASHTAG TEXT NOT NULL,
+    ID INT NOT NULL,
+    FOREIGN KEY (ID) REFERENCES TWEETS(ID),
+    PRIMARY KEY (HASHTAG, ID));''')
 
-if verbose : print "Table Hashtags created successfully"
+if verbose:
+    print "Table Hashtags created successfully"
 
 conn.execute('''CREATE TABLE URLS
-       (URL TEXT NOT NULL,
-        ID INT NOT NULL,
-	FOREIGN KEY (ID) REFERENCES TWEETS(ID),
-        PRIMARY KEY (URL, ID));''')
+    (URL TEXT NOT NULL,
+    ID INT NOT NULL,
+    FOREIGN KEY (ID) REFERENCES TWEETS(ID),
+    PRIMARY KEY (URL, ID));''')
 
-if verbose : print "Table Hashtags created successfully"
+if verbose:
+    print "Table Hashtags created successfully"
 
 # Creating tables for relations between main tables
 # PRODUCES --> A user produces many tweets
 conn.execute('''CREATE TABLE PRODUCES
-       (ID_TWEET INT NOT NULL,
-        ID_USER INT NOT NULL,
-	FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID),
-	FOREIGN KEY (ID_USER) REFERENCES USERS(ID),
-        PRIMARY KEY (ID_USER, ID_TWEET));''')
+    (ID_TWEET INT NOT NULL,
+    ID_USER INT NOT NULL,
+    FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID),
+    FOREIGN KEY (ID_USER) REFERENCES USERS(ID),
+    PRIMARY KEY (ID_USER, ID_TWEET));''')
 
-if verbose : print "Table Produces created successfully"
+if verbose:
+    print "Table Produces created successfully"
 
 # MENTIONS --> A tweet mentions many users
 conn.execute('''CREATE TABLE MENTIONS
-       (ID_TWEET INT NOT NULL,
-        ID_USER INT NOT NULL,
-	FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID),
-	FOREIGN KEY (ID_USER) REFERENCES USERS(ID),
-        PRIMARY KEY (ID_USER, ID_TWEET));''')
+    (ID_TWEET INT NOT NULL,
+    ID_USER INT NOT NULL,
+    FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID),
+    FOREIGN KEY (ID_USER) REFERENCES USERS(ID),
+    PRIMARY KEY (ID_USER, ID_TWEET));''')
 
-if verbose : print "Table Mentions created successfully"
+if verbose:
+    print "Table Mentions created successfully"
 
 # Creating indexes
 # Index to search easily by geo-position
 conn.execute("CREATE INDEX GEO ON TWEETS(LAT, LONG);")
 
-if verbose : print "Index GEO created successfully"
+if verbose:
+    print "Index GEO created successfully"
 
 # Closing the connection
 conn.close()
