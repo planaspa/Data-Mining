352314f7a6354267edb3fead2c453ff57e5bf66b
planaspa <planaspa@gmail.com>
2014-11-28 22:52:01 +0100

    DataBase Creator tested and finished 100%

--- src/dbCreator.py
+++ src/dbCreator.py
@@ -1,115 +1,139 @@
 import sqlite3
 import sys
 
-# Verbose mode activation
-if "-v" in sys.argv:
-    verbose = True
-    print "Verbose mode ON."
-else:
-    verbose = False
-    print "Verbose mode OFF. To activate it use -v parametter when excecuting"
+verbose = False
 
+def tweetsTableCreator(conn):
+    # Creating main tables
+    conn.execute('''CREATE TABLE TWEETS
+        (ID INT PRIMARY KEY NOT NULL,
+        TWEET_TEXT CHAR(140) NOT NULL,
+        FAVS INT NOT NULL,
+        RTS INT NOT NULL,
+        LAT REAL,
+        LONG REAL,
+        FOLLOWERS INT);''')
 
-# Connection to DataBase
-conn = sqlite3.connect('db/tweetBank.db')
-if verbose:
-    print "Opened database successfully"
+    if verbose:
+        print "Table Tweets created successfully"
 
-# Creating main tables
-conn.execute('''CREATE TABLE TWEETS
-    (ID INT PRIMARY KEY NOT NULL,
-    TWEET_TEXT CHAR(140) NOT NULL,
-    FAVS INT NOT NULL,
-    RTS INT NOT NULL,
-    LAT REAL,
-    LONG REAL,
-    FOLLOWERS INT);''')
+def usersTableCreator(conn):
+    # If verified equals to one means it is true, zero means it is false
+    conn.execute('''CREATE TABLE USERS
+        (ID INT PRIMARY KEY NOT NULL,
+        SCREEN_NAME TEXT NOT NULL,
+        NAME TEXT NOT NULL,
+        VERIFIED INT NOT NULL,
+        LANG TEXT);''')
 
-if verbose:
-    print "Table Tweets created successfully"
+    if verbose:
+        print "Table Users created successfully"
 
-# If verified equals to one means it is true, zero means it is false
-conn.execute('''CREATE TABLE USERS
-    (ID INT PRIMARY KEY NOT NULL,
-    SCREEN_NAME TEXT NOT NULL,
-    NAME TEXT NOT NULL,
-    VERIFIED INT NOT NULL,
-    LANG TEXT);''')
+def timeTableCreator(conn):
+    # Table for tweets timing
+    conn.execute('''CREATE TABLE TIME
+        (ID_TWEET INT PRIMARY KEY NOT NULL,
+        DAY_OF_THE_WEEK TEXT NOT NULL,
+        DAY INT NOT NULL,
+        MONTH TEXT NOT NULL,
+        HOUR INT NOT NULL,
+        MINUTE INT NOT NULL,
+        SECOND INT NOT NULL,
+        FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID));''')
 
-if verbose:
-    print "Table Users created successfully"
+    if verbose:
+        print "Table Time created successfully"
 
-# Table for tweets timing
-conn.execute('''CREATE TABLE TIME
-    (ID_TWEET INT PRIMARY KEY NOT NULL,
-    DAY_OF_THE_WEEK TEXT NOT NULL,
-    DAY INT NOT NULL,
-    MONTH TEXT NOT NULL,
-    HOUR INT NOT NULL,
-    MINUTE INT NOT NULL,
-    SECOND INT NOT NULL,
-    FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID));''')
+def hashtagsTableCreator(conn):
+    # Creating tables for multivaluated attributes for some tables
+    conn.execute('''CREATE TABLE HASHTAGS
+        (HASHTAG TEXT NOT NULL COLLATE NOCASE,
+        ID INT NOT NULL,
+        FOREIGN KEY (ID) REFERENCES TWEETS(ID),
+        PRIMARY KEY (HASHTAG, ID));''')
 
-if verbose:
-    print "Table Time created successfully"
+    if verbose:
+        print "Table Hashtags created successfully"
 
-# Creating tables for multivaluated attributes for some tables
-conn.execute('''CREATE TABLE HASHTAGS
-    (HASHTAG TEXT NOT NULL COLLATE NOCASE,
-    ID INT NOT NULL,
-    FOREIGN KEY (ID) REFERENCES TWEETS(ID),
-    PRIMARY KEY (HASHTAG, ID));''')
+def urlsTableCreator(conn):
+    conn.execute('''CREATE TABLE URLS
+        (URL TEXT NOT NULL,
+        ID INT NOT NULL,
+        FOREIGN KEY (ID) REFERENCES TWEETS(ID),
+        PRIMARY KEY (URL, ID));''')
 
-if verbose:
-    print "Table Hashtags created successfully"
+    if verbose:
+        print "Table Hashtags created successfully"
 
-conn.execute('''CREATE TABLE URLS
-    (URL TEXT NOT NULL,
-    ID INT NOT NULL,
-    FOREIGN KEY (ID) REFERENCES TWEETS(ID),
-    PRIMARY KEY (URL, ID));''')
+def wordTableCreator(conn):
+    conn.execute('''CREATE TABLE WORDS
+        (WORD TEXT NOT NULL COLLATE NOCASE,
+        ID INT NOT NULL,
+        FOREIGN KEY (ID) REFERENCES TWEETS(ID),
+        PRIMARY KEY (WORD, ID));''')
 
-if verbose:
-    print "Table Hashtags created successfully"
+    if verbose:
+        print "Table Words created successfully"
 
-conn.execute('''CREATE TABLE WORDS
-    (WORD TEXT NOT NULL COLLATE NOCASE,
-    ID INT NOT NULL,
-    FOREIGN KEY (ID) REFERENCES TWEETS(ID),
-    PRIMARY KEY (WORD, ID));''')
+def producesTableCreator(conn):
+    # PRODUCES --> A user produces many tweets
+    conn.execute('''CREATE TABLE PRODUCES
+        (ID_TWEET INT NOT NULL,
+        ID_USER INT NOT NULL,
+        FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID),
+        FOREIGN KEY (ID_USER) REFERENCES USERS(ID),
+        PRIMARY KEY (ID_USER, ID_TWEET));''')
 
-if verbose:
-    print "Table Words created successfully"
+    if verbose:
+        print "Table Produces created successfully"
 
-# Creating tables for relations between main tables
-# PRODUCES --> A user produces many tweets
-conn.execute('''CREATE TABLE PRODUCES
-    (ID_TWEET INT NOT NULL,
-    ID_USER INT NOT NULL,
-    FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID),
-    FOREIGN KEY (ID_USER) REFERENCES USERS(ID),
-    PRIMARY KEY (ID_USER, ID_TWEET));''')
+def mentionsTableCreator(conn):
+    # MENTIONS --> A tweet mentions many users
+    conn.execute('''CREATE TABLE MENTIONS
+        (ID_TWEET INT NOT NULL,
+        ID_USER INT NOT NULL,
+        FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID),
+        FOREIGN KEY (ID_USER) REFERENCES USERS(ID),
+        PRIMARY KEY (ID_USER, ID_TWEET));''')
 
-if verbose:
-    print "Table Produces created successfully"
+    if verbose:
+        print "Table Mentions created successfully"
 
-# MENTIONS --> A tweet mentions many users
-conn.execute('''CREATE TABLE MENTIONS
-    (ID_TWEET INT NOT NULL,
-    ID_USER INT NOT NULL,
-    FOREIGN KEY (ID_TWEET) REFERENCES TWEETS(ID),
-    FOREIGN KEY (ID_USER) REFERENCES USERS(ID),
-    PRIMARY KEY (ID_USER, ID_TWEET));''')
+def indexCreator(conn):
+    # Index to search easily by geo-position
+    conn.execute("CREATE INDEX GEO ON TWEETS(LAT, LONG);")
 
-if verbose:
-    print "Table Mentions created successfully"
+    if verbose:
+        print "Index GEO created successfully"
 
-# Creating indexes
-# Index to search easily by geo-position
-conn.execute("CREATE INDEX GEO ON TWEETS(LAT, LONG);")
+if __name__ == "__main__":
 
-if verbose:
-    print "Index GEO created successfully"
+    # Verbose mode activation
+    if "-v" in sys.argv:
+        verbose = True
+        print "Verbose mode ON."
+    else:
+        verbose = False
+        print "Verbose mode OFF. To activate it use -v parametter when excecuting"
 
-# Closing the connection
-conn.close()
+    # Connection to DataBase
+    con = sqlite3.connect('db/tweetBank.db')
+    if verbose:
+        print "Opened database successfully"
+
+    # Creating main tables
+    tweetsTableCreator(con)
+    usersTableCreator(con)
+
+    # Creating tables for relations between main tables
+    timeTableCreator(con)
+    hashtagsTableCreator(con)
+    urlsTableCreator(con)
+    wordTableCreator(con)
+    producesTableCreator(con)
+    mentionsTableCreator(con)
+
+    # Creating indexes
+    indexCreator(con)
+    # Closing the connection
+    conn.close()
---
+++ test_dbCreator.py
@@ -0,0 +1,140 @@
+from pytest import raises
+from src.dbCreator import *
+import os
+
+db = 'db/test.db'
+
+def test_tweetsTableCreator():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+
+    tweetsTableCreator(conn)
+    c.execute ("SELECT count(*) FROM sqlite_master "
+            "WHERE type = 'table' and name = 'TWEETS'")
+
+    result = c.fetchone()
+    assert result[0] == 1
+
+    # Closing the connection
+    conn.close()
+
+def test_usersTableCreator():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+
+    usersTableCreator(conn)
+    c.execute ("SELECT count(*) FROM sqlite_master "
+            "WHERE type = 'table' and name = 'USERS'")
+
+    result = c.fetchone()
+    assert result[0] == 1
+
+    # Closing the connection
+    conn.close()
+
+def test_timeTableCreator():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+
+    timeTableCreator(conn)
+    c.execute ("SELECT count(*) FROM sqlite_master "
+            "WHERE type = 'table' and name = 'TIME'")
+
+    result = c.fetchone()
+    assert result[0] == 1
+
+    # Closing the connection
+    conn.close()
+
+def test_hashtagsTableCreator():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+
+    hashtagsTableCreator(conn)
+    c.execute ("SELECT count(*) FROM sqlite_master "
+            "WHERE type = 'table' and name = 'HASHTAGS'")
+
+    result = c.fetchone()
+    assert result[0] == 1
+
+    # Closing the connection
+    conn.close()
+
+def test_urlsTableCreator():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+
+    urlsTableCreator(conn)
+    c.execute ("SELECT count(*) FROM sqlite_master "
+            "WHERE type = 'table' and name = 'URLS'")
+
+    result = c.fetchone()
+    assert result[0] == 1
+
+    # Closing the connection
+    conn.close()
+
+def test_wordTableCreator():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+
+    wordTableCreator(conn)
+    c.execute ("SELECT count(*) FROM sqlite_master "
+            "WHERE type = 'table' and name = 'WORDS'")
+
+    result = c.fetchone()
+    assert result[0] == 1
+
+    # Closing the connection
+    conn.close()
+
+def test_producesTableCreator():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+
+    producesTableCreator(conn)
+    c.execute ("SELECT count(*) FROM sqlite_master "
+            "WHERE type = 'table' and name = 'PRODUCES'")
+
+    result = c.fetchone()
+    assert result[0] == 1
+
+    # Closing the connection
+    conn.close()
+
+def test_mentionsTableCreator():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+
+    mentionsTableCreator(conn)
+    c.execute ("SELECT count(*) FROM sqlite_master "
+            "WHERE type = 'table' and name = 'MENTIONS'")
+
+    result = c.fetchone()
+    assert result[0] == 1
+
+    # Closing the connection
+    conn.close()
+
+def test_indexCreator():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+
+    indexCreator(conn)
+    c.execute ("SELECT count(*) FROM sqlite_master"
+            " WHERE type = 'index' and name = 'GEO'")
+
+    result = c.fetchone()
+    assert result[0] == 1
+
+    # Closing the connection
+    conn.close()
+
+def test_removeDB():
+    """
+    This method is not in the original module. But it is neccesary 
+    if we want to remove the db for the followings tests.
+    """
+    os.remove(db)
+
+
