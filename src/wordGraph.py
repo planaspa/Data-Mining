88d5cbeb10a62e63e9e26710673ffd03ef6d1a12
planaspa <planaspa@gmail.com>
2014-11-29 23:10:12 +0100

    WordGraph tested and 100% implemented: 	- It goes really slow with a big amount of data, but checking all 	 the words are making connections between them should take a long 	 time...

--- src/wordGraph.py
+++ src/wordGraph.py
@@ -29,76 +29,89 @@ def create_position (matrix, wordA, wordB):
     matrix [wordA] [wordB] = 0
     return [wordA, wordB]
     
-def bound (): 
+def bound (c, argument): 
     c.execute("SELECT count(*) FROM TWEETS")
     result = c.fetchone()
     tweets = result[0]
-    return tweets * float(sys.argv[1])
+    return tweets * (1 - float(argument))
 
-if len(sys.argv) != 2:
-    print "I need a percentage (number between 0 and 1) as an argument"
-    sys.exit()
+def readTweets(c, argument):
+    # We select all the tweets with word repetition above the bound in the db
+    c.execute("SELECT DISTINCT(ID) FROM WORDS WHERE WORD IN ("+
+              "SELECT WORD FROM (SELECT WORD, COUNT(*) AS TOTAL FROM WORDS " +
+              "GROUP BY WORD HAVING TOTAL > " + str(bound(c, argument))+ "))");
+    tweet_list = [record[0] for record in c.fetchall()]
+    return tweet_list
+
+def matchWords(c, tweet_list):
+
+    # Creation of the matrix
+    word_matrix = defaultdict(dict)
+
+    # For each tweet we see the word that it contains, and we match them
+    # in the matrix
+    for tweet in tweet_list:
+        c.execute ("SELECT WORD FROM WORDS WHERE ID =%i"
+                   % (tweet))
+        words = [text_format(record[0]) for record in c.fetchall()]
+        
+        for word in words:
+            words.remove(word)
+            for rest_of_the_words in words:
+                # We find the correct position for the word in the matrix
+                correct_positions = correct_position (word_matrix, word, rest_of_the_words)
+                if correct_positions is None:
+                    # If the words are not in the matrix yet, we add them
+                    correct_positions = create_position (word_matrix, word, rest_of_the_words)
+                word_matrix [correct_positions[0]] [correct_positions[1]] += 1
     
+    return word_matrix
 
-conn = sqlite3.connect('db/tweetBank.db')
 
-c = conn.cursor()
+if __name__ == "__main__":
 
-# Creation of the matrix
-word_matrix = defaultdict(dict)
+    if len(sys.argv) != 2:
+        print "I need a percentage (number between 0 and 1) as an argument"
+        sys.exit()
+        
 
-print "Reading tweets from the database..."
+    conn = sqlite3.connect('db/tweetBank.db')
+    c = conn.cursor()
 
-# We select all the tweets with word repetition above the bound in the db
-c.execute("SELECT DISTINCT(ID) FROM WORDS WHERE WORD IN ("+
-          "SELECT WORD FROM (SELECT WORD, COUNT(*) AS TOTAL FROM WORDS " +
-          "GROUP BY WORD HAVING TOTAL > " + str(bound())+ "))");
-tweet_list = [record[0] for record in c.fetchall()]
+    print "Reading tweets from the database..."
 
-print "Reading words from the database..."
+    tweet_list = readTweets(c, sys.argv[1])
 
-# For each tweet we see the word that it contains, and we match them
-# in the matrix
-for tweet in tweet_list:
-    c.execute ("SELECT WORD FROM WORDS WHERE ID =%i"
-               % (tweet))
-    words = [text_format(record[0]) for record in c.fetchall()]
-    
-    for word in words:
-        words.remove(word)
-        for rest_of_the_words in words:
-            # We find the correct position for the word in the matrix
-            correct_positions = correct_position (word_matrix, word, rest_of_the_words)
-            if correct_positions is None:
-                # If the words are not in the matrix yet, we add them
-                correct_positions = create_position (word_matrix, word, rest_of_the_words)
-            word_matrix [correct_positions[0]] [correct_positions[1]] += 1
+    print "Reading words from the database..."
 
-print "Creating the graph..."
+    word_matrix = matchWords(c, tweet_list)
 
-graph = nx.Graph()
-edge_labels = {}
+    print "Creating the graph..."
 
-for word in word_matrix:
-    for other_word in word_matrix[word]:
-        if word_matrix[word][other_word] > bound():
-            graph.add_edge(word,other_word)
-            graph.node[other_word]['word']=other_word
-            edge_labels [(word,other_word)] = word_matrix[word][other_word]
-            # print ("%s + %s = %i" %(word, other_word, word_matrix[word][other_word] ))
-    try:    
-        graph.node[word]['word']=word
-    except:
-        pass
+    graph = nx.Graph()
+    edge_labels = {}
+    bound = bound(c, sys.argv[1])
 
-# Closing the connection
-conn.close()
+    for word in word_matrix:
+        for other_word in word_matrix[word]:
+            if word_matrix[word][other_word] > bound:
+                graph.add_edge(word,other_word)
+                graph.node[other_word]['word']= other_word
+                edge_labels [(word,other_word)] = word_matrix[word][other_word]
+                # print ("%s + %s = %i" %(word, other_word, word_matrix[word][other_word] ))
+        try:    
+            graph.node[word]['word']=word
+        except:
+            pass
 
-pos=nx.spring_layout(graph)
+    # Closing the connection
+    conn.close()
 
-nx.draw(graph, pos)
-nx.draw_networkx_edge_labels(graph,pos,edge_labels=edge_labels)
-node_labels = nx.get_node_attributes(graph,'word')
-nx.draw_networkx_labels(graph, pos, labels = node_labels)
+    pos=nx.spring_layout(graph)
 
-plt.show()
+    nx.draw(graph, pos)
+    nx.draw_networkx_edge_labels(graph,pos,edge_labels=edge_labels)
+    node_labels = nx.get_node_attributes(graph,'word')
+    nx.draw_networkx_labels(graph, pos, labels = node_labels)
+
+    plt.show()
